950498adb483812a38648278ff27d0bdb0f03901
Achintha Pallegedara <achinthahirudika@gmail.com>
2024-11-14 13:49:15 +0530

    add next config to host domain

--- next.config.ts
+++ next.config.ts
@@ -15,7 +15,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:3001/api/:path*",
+        destination: "https://flashmelk-dashboard.vercel.app/api/:path*",
       },
     ];
   },
